94a02f29eb3859290ecd44618d3d6fdaba6a1f48
Silver267 <jxuanxi@gmail.com>
2023-05-12 11:52:24 -0400

    Initialization process

---
+++ ICS4UFinal-Ponyville/Node.java
@@ -0,0 +1,15 @@
+/**
+ * Node, the building block of map
+ * 
+ * @author Thomas & George (refactor)
+ * @version 1.0
+ */
+public class Node{
+    public Node(int type){
+        
+    }
+    
+    public int getT(){
+        return 0;
+    }
+}
