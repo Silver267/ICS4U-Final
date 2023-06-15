68583fa80453407f76d606c0179af6afa76abfb8
349225219 <101135430+349225219@users.noreply.github.com>
2023-06-14 20:19:07 -0400

    more change

---
+++ ICS4UFinal-Ponyville/Option.java
@@ -0,0 +1,41 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Option here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Option extends BattleScreen
+{
+    private GreenfootImage[] animation;
+    private boolean startCount;
+    private int count;
+    //int check meant to check if the option is a continue button
+    public Option(GreenfootImage gf, boolean check){
+        animation = new GreenfootImage[1];
+        animation[0] = gf;
+        
+        if(check){
+            animation[0].scale(85, 85);
+        }else{
+            animation[0].scale(110, 110);
+        }
+        
+        setImage(gf);
+    }
+    /**
+     * Act - do whatever the Option wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        
+    }
+    
+    public boolean isClick(){
+        startCount = true;
+        return Greenfoot.mouseClicked(this);
+    }
+    
+}
