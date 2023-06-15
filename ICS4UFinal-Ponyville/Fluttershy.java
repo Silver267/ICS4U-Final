aa7f1201e144d540744494b951c9e9501a63baed
Silver267 <105079286+Silver267@users.noreply.github.com>
2023-06-15 19:08:48 -0400

    push

--- ICS4UFinal-Ponyville/Fluttershy.java
+++ ICS4UFinal-Ponyville/Fluttershy.java
@@ -1,27 +1,33 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
 
 /**
- * Write a description of class PonyBoss here.
+ * Write a description of class PinkiePie here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Fluttershy extends Enemy{
     /**
-     * Act - do whatever the PonyBoss wants to do. This method is called whenever
+     * Act - do whatever the PinkiePie wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    private int x, cnt;
-    private double prevAng;
+    private int x, meth, timer, prevAng, cnt;
+    private double ang; 
+    private SideWorld sw;
     
-    public Fluttershy(){
-        cnt = 0;
-        x = 0;
-        prevAng = 0;
+    //meth: which attack pattern will use 
+    private GreenfootImage gf;
+    public Fluttershy(int meth, int duration){
+        x = 0; this.meth = meth;
+        cnt = 0; ang = 0; timer = duration;
+        gf = new GreenfootImage("MainPony/FS-1.png");
+        gf.scale(150, 150);
+        setImage(gf);
     }
     
-    private double phase1Method1(double dx, double ang){//method to generate the bullets of phase 1
-        double fx = ((-1/10.0)*dx + 28/3.0); //generation (angle) function
+    private void phase1Method1(){
+        double fx = ((-1/10.0)*x + 28/3.0); //generation (angle) function
         ang = ang+fx;
         int xpos = this.getX(), ypos = this.getY();
         for(int i=0; i<4; i++){//function to generate bullet for first spell card.
@@ -29,26 +35,34 @@ public class Fluttershy extends Enemy{
             //set speed to 3.46 + x/584.0 so that the speed of bullets are increasing.
             getWorld().addObject(new Bullet_Undirected(0, (3.46+x/584.0), (int)(ang+theta), 8, 17, xpos, ypos, false), xpos, ypos);
         }
-        return ang;
     }
-
-    private void phase1ATK(){ //Attack method for phase1 (each progression is calculated by frames)
-        if(cnt<=0){
-            cnt = 1;
-            prevAng = phase1Method1(x, prevAng);
-            x++;
+    
+    private void phase1ATK(){
+        if(cnt==0){
+            phase1Method1();
+            x++; cnt = 1;
         }else{
             cnt--;
         }
     }
     
-    
-    private void die(){
-        
+    public void addedToWorld(World w){
+        sw = (SideWorld)w;
     }
     
     public void act(){
-        phase1ATK();
-        // Add your action code here.
+        timer--;
+        switch(meth){
+            case 1:
+                phase1ATK();
+                break;
+        }
+        if(timer==0){
+            sw.changeTalk(true);
+            ((SideWorld)getWorld()).remAllBullets();
+            
+            getWorld().removeObject(this);
+        }
+            
     }
 }
